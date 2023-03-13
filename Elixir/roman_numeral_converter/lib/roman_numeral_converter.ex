88c3f1e48670ba2e3e9cefe972c2a58d20411dc8
Chris Johnston <cjohnston@connected.io>
2023-03-13 12:47:18 -0400

    elixir: tests for unique roman numerals working, refactored to map

--- Elixir/roman_numeral_converter/lib/roman_numeral_converter.ex
+++ Elixir/roman_numeral_converter/lib/roman_numeral_converter.ex
@@ -1,18 +1,29 @@
 defmodule RomanNumeralConverter do
   @moduledoc """
-  Documentation for `RomanNumeralConverter`.
+  Provides functions for converting to and from Roman numerals
   """
+
+  @roman_numerals %{
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+    "L" => 50,
+    "C" => 100,
+    "D" => 500,
+    "M" => 100,
+  }
 
   @doc """
-  To Integer
-
-  ## Examples
-
-      iex> RomanNumeralConverter.to_integer(input)
-      :world
-
+  Convert a roman numeral to an integer
   """
   def to_integer(input) do
-    :integer
+    @roman_numerals[input]
+  end
+
+  @doc """
+  Convert an integer to a roman numeral
+  """
+  def to_roman(input) do
+    :roman
   end
 end
--- Elixir/roman_numeral_converter/test/roman_numeral_converter_test.exs
+++ Elixir/roman_numeral_converter/test/roman_numeral_converter_test.exs
@@ -1,8 +1,19 @@
 defmodule RomanNumeralConverterTest do
   use ExUnit.Case
-  doctest RomanNumeralConverter
 
-  test "greets the world" do
-    assert RomanNumeralConverter.to_integer("") == :integer
+  test "should return 1 when given I" do
+    assert RomanNumeralConverter.to_integer("I") == 1
   end
+
+  test "should return 5 when given V" do
+    assert RomanNumeralConverter.to_integer("V") == 5
+  end
+
+  test "should return 10 when given X" do
+    assert RomanNumeralConverter.to_integer("X") == 10
+  end
+
+  # test "should return 2 when given II" do
+  #   assert RomanNumeralConverter.to_integer("II") == 2
+  # end
 end
